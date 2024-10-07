97fb9c84db749309ba05c9584586a11fe070e590
Jordan Junaidi <jordanrjunaidi@gmail.com>
2024-10-06 23:03:14 -0700

    Add script.js
    script.js manages the calculator's operations as well as updating the
    display text of the calculator at all times.

---
+++ script.js
@@ -0,0 +1,72 @@
+let numbers = [];
+let operations = [];
+
+function updateText(text) {
+    const display = document.querySelector("#display");
+    display.textContent = display.textContent + text;
+}
+
+function clearText() {
+    const display = document.querySelector("#display");
+    display.textContent = "";
+}
+
+function operate(nums, operations) {
+    console.log(nums);
+    console.log(operations);
+
+    if (nums.length != operations.length+1) {
+        clearText();
+        updateText("ERROR. PLEASE CLEAR");
+
+    }
+
+    else {
+
+        let total=parseInt(nums[0]);
+        for (let i=1; i<nums.length; i++) {
+            if (operations[i-1] === "+") {
+                total = total + parseInt(nums[i]);
+            }
+            else if (operations[i-1] === "-") {
+                total = total - parseInt(nums[i]);
+            }
+            else if (operations[i-1] === "x") {
+                total = total * parseInt(nums[i]);
+            }
+            else {
+                total = total / parseInt(nums[i]);
+            }
+        }
+
+        clearText();
+        updateText(`${total}`);
+    }   
+}
+
+addEventListener("click", (e) => {
+    if (e.target.tagName === "BUTTON") {
+
+        if (e.target.className === "number") {
+            updateText(e.target.id);
+        }
+        else {
+            updateText(e.target.id);
+            if (e.target.id === '=') {
+                numbers = document.querySelector("#display").textContent.slice(0, -1);
+                numbers = numbers.replace(/[-x\+\/]/g, ' ').trim();
+                numbers = numbers.split(' ');
+                operate(numbers, operations);
+                operations = [];
+            }
+            else if (e.target.id === "clear") {
+                numbers = [];
+                operations = [];
+                clearText();
+            }
+            else {
+                operations.push(e.target.id);
+            }
+        }
+    }   
+});
